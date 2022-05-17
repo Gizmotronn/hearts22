67321addcb0016b408c4bbc5b96e2b9b4023fb99
Liam Arbuckle <droidology@allianceofdroids.org.au>
2022-05-17 21:29:21 +0800

    ⚰️💣 6 more fan comments added

---
+++ src/components/sections/Roadmap/RoadmapPartTwo.js
@@ -0,0 +1,230 @@
+import React, { useLayoutEffect, useRef } from "react";
+import styled from 'styled-components';
+import DrawSvg from './DrawSvg';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.body};
+  position: relative;
+  display: inline-block;
+  overflow: hidden;
+
+`;
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxl};
+  color: ${(props) => props.theme.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem auto;
+  border-bottom: 2px solid ${(props) => props.theme.text};
+  width: fit-content;
+
+  @media (max-width: 40em) {
+    font-size: ${(props) => props.theme.fontxl};
+  }
+`;
+const Container = styled.div`
+  width: 70%;
+  height: 200vh;
+  background-color: ${(props) => props.theme.body};
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  @media (max-width: 64em) {
+    width: 80%;
+  }
+  @media (max-width: 48em) {
+    width: 90%;
+  }
+`;
+const SvgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Items = styled.ul`
+  list-style: none;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* background-color: lightblue; */
+
+  @media (max-width: 48em) {
+    width: 90%;
+  }
+
+  & > *:nth-of-type(2n + 1) {
+    justify-content: start;
+    @media (max-width: 48em) {
+      justify-content: center;
+    }
+
+    div {
+      border-radius: 50px 0 50px 0;
+      text-align: right;
+
+      @media (max-width: 48em) {
+        border-radius: 0 50px 0 50px;
+      text-align: left;
+        p {
+          border-radius: 0 40px 0 40px;
+
+        }
+      }
+    }
+    p {
+      border-radius: 40px 0 40px 0;
+    }
+  }
+  & > *:nth-of-type(2n) {
+    justify-content: end;
+    @media (max-width: 48em) {
+      justify-content: center;
+    }
+    div {
+      border-radius: 0 50px 0 50px;
+      text-align: left;
+
+      
+    }
+    p {
+      border-radius: 0 40px 0 40px;
+    }
+  }
+`;
+const Item = styled.li`
+  width: 100%;
+  height: 100%;
+  display: flex;
+
+  @media (max-width: 48em) {
+    justify-content: flex-end !important;
+  }
+`;
+const ItemContainer = styled.div`
+  width: 40%;
+  height: fit-content;
+  padding: 1rem;
+  border: 3px solid ${(props) => props.theme.text};
+
+  @media (max-width: 48em) {
+    width: 70%;
+
+  }
+`;
+
+const Box = styled.p`
+  height: fit-content;
+  background-color: ${(props) => props.theme.carouselColor};
+  color: ${(props) => props.theme.text};
+  padding: 1rem;
+  position: relative;
+  border: 1px solid ${(props) => props.theme.text};
+`;
+const SubTitle = styled.span`
+  display: block;
+  font-size: ${(props) => props.theme.fontxl};
+  color: ${(props) => props.theme.text};
+
+  @media (max-width: 40em) {
+    font-size: ${(props) => props.theme.fontlg};
+    font-weight: 600;
+  }
+`;
+const Text = styled.span`
+  display: block;
+  font-size: ${(props) => props.theme.fontsm};
+  color: ${(props) => props.theme.text};
+
+  font-weight: 400;
+  margin: 0.5rem 0;
+  @media (max-width: 40em) {
+    font-size: ${(props) => props.theme.fontxs};
+  }
+`;
+
+const RoadMapItem = ({ title, subtext, addToRef }) => {
+  return (
+    <Item ref={addToRef}>
+      <ItemContainer>
+        <Box>
+          <SubTitle>{title} </SubTitle>
+          <Text>{subtext}</Text>
+        </Box>
+      </ItemContainer>
+    </Item>
+  );
+};
+
+const RoadmapPartTwo = () => {
+  const revealRefs = useRef([]);
+  revealRefs.current = [];
+  gsap.registerPlugin(ScrollTrigger);
+
+  const addToRefs = (el) => {
+    if (el && !revealRefs.current.includes(el)) {
+      revealRefs.current.push(el);
+    }
+  };
+
+  useLayoutEffect(() => {
+    let t1 = gsap.timeline();
+    revealRefs.current.forEach((el, index) => {
+      t1.fromTo(
+        el.childNodes[0],
+        {
+          y: "0",
+        },
+        {
+          y: "-30%",
+
+          scrollTrigger: {
+            id: `section-${index + 1}`,
+            trigger: el,
+            start: "top center+=200px",
+            end: "bottom center",
+            scrub: true,
+            // markers:true,
+          },
+        }
+      );
+    });
+
+    return () => {
+      if (t1) t1.kill();
+    };
+  }, []);
+
+    return (
+        <Section>
+            <Title>What it means....TO US</Title>
+            <Container>
+                <SvgContainer>
+                    <DrawSvg />
+                </SvgContainer>
+                <Items>
+                    <Item>&nbsp;</Item> {/* Empty item */}
+                        <RoadMapItem addToRef={addToRefs} title="Matty Norwood" subtext="6am pints at Tulla, 12 hour drives learning about the band anal cunt, strip clubs in Adelaide. Having the time of my life with my best mates while following a team that used always break our hearts" />
+                        <RoadMapItem addToRef={addToRefs} title="Zak Birznieks" subtext="This club means the world to me, the smaller attendance has made it more of a close family for those in active! Away days with the Active lads have been some of the best trips of my life." />
+                        <RoadMapItem addToRef={addToRefs} title="James Penney" subtext="Singing for 90 mins where everybody from all different walks of life comes together for that one cause." />
+                        <RoadMapItem addToRef={addToRefs} title="Jessee Jenkinson" subtext="Always for Melbourne ❤️🤍💙 Great club with some of the most loyal fans" />
+                        <RoadMapItem addToRef={addToRefs} title="Anon" subtext="My whole family are Victory supporters, and although I started following City out of spite, my passion for the sport and club outgrew the spite. Fuck the Victards" />
+                        <RoadMapItem addToRef={addToRefs} title="Goce Todoroski" subtext="taking my nephew to his 1st game 7 years ago when we beat Perth Glory 5-1... now 7 years later we havent missed a game and have turned our whole family of 10 into passionate city fans/members and are always behind the boys" />
+                </Items>
+            </Container>
+        </Section>
+    )
+}
+
+export default RoadmapPartTwo;
