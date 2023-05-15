824b0f0505d0b1e7fb8f3f49e1287bf7edd29687
Liam Arbuckle <liam@skinetics.tech>
2023-05-16 05:38:23 +1000

    🍅🌌 ↝ Turns out the init dir was causing all the problems, not ventura

--- init/components/gridGallery.tsx
+++ init/components/gridGallery.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Image from 'next/image';
 
 const GridGallery: React.FC = () => {
-  const photoCount = 18;
+  const imageCount = 18; // Number of images in the gallery
+
+  const getImagePath = (index: number): string => `/players/${index}.png`;
 
   return (
     <div className="grid grid-cols-3 gap-4">
-      {Array.from(Array(photoCount), (_, index) => (
+      {Array.from(Array(imageCount), (_, index) => (
         <div key={index} className="relative aspect-w-1 aspect-h-1">
           <Image
-            src={`/players/${index + 1}.png`}
+            src={getImagePath(index + 1)}
             alt={`Player ${index + 1}`}
             layout="fill"
             objectFit="cover"
---
+++ init/pages/tests/gallery.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GridGallery from "../../components/gridGallery";
+
+export default function GalleryTest () {
+    return (
+        <div className="py-16 bg-gray-100">
+            <div className="container mx-auto">
+                <GridGallery />
+            </div>
+        </div>
+    )
+}
